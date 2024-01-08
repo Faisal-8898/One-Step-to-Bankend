9602cdd8e9a10abe7604cfcd36acf372eeac1c31
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-01-08 17:22:06 +0600

    .

--- javascript/01_conversion.js
+++ javascript/01_conversion.js
@@ -1,18 +1,42 @@
-const str = "34a";
+// function convertToNumber(value) {
+//   if (value === null || value === undefined) {
+//     return { status: "Value is null or undefined", number: null };
+//   }
+//
+//   const number = Number(value);
+//
+//   if (isNaN(number)) {
+//     return { status: "Cannot be converted to a number", number: null };
+//   }
+//
+//   return { status: "Successful conversion", number: number };
+// }
 
-const convertedIntoNumber = checkIsItNumberOrNot(str);
+const { words } = require("lodash");
 
-if (convertedIntoNumber.convertedNumber === null) {
-} else {
-}
+let age = "33a";
+let temp = null;
 
-function checkIsItNumberOrNot(value) {
-  if (value === null || value === undefined) {
-    return { statusMsg: "not a valid Number", convertedNumber: null };
-  }
-  const convertedIntoNumber = Number(str);
-  if (isNaN(convertedIntoNumber)) {
-    return { statusMsg: "not a valid Number", convertedNumber: null };
-  }
-  return { statusMsg: "valid Number", convertedNumber: convertedIntoNumber };
-}
+// const str = new String("hello world i am object"); // thing that its array [0]
+// console.log(str.valueOf());
+
+let numa = 34;
+
+const numaString = String(numa);
+
+// console.log(numaString);
+// console.log(typeof numaString);
+//
+const valueInNumber = Number(age);
+const tempInNumber = Number(temp);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// console.log(typeof tempInNumber);
+// console.log(tempInNumber);
+
+// 3 -> 3
+// "33" -> 33
+// "33abc" -> NaN
+// "abc" -> NaN
+// null -> 0
