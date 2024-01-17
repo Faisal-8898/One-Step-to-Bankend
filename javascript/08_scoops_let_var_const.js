e05efc6829536928be991493411883c6cf21cfb8
Faisal Ahmed <faisalahmed8988@gmail.com>
2024-01-18 00:45:36 +0600

    js execution context and let, var, const loop

---
+++ javascript/08_scoops_let_var_const.js
@@ -0,0 +1,81 @@
+// if(true){
+//
+// }
+//
+// function foo(){
+//
+// }
+//
+// const fa = () => {
+//
+// }
+//
+// for (let i = 0; i < 10; i++) {
+// }
+//
+// function hello() {
+//   var me = "faisal";
+// }
+// hello();
+// console.log(me);
+//
+// if (true) {
+//   var me = "faisal";
+// }
+//
+// console.log(me);
+
+// const hello = "hello";
+// {}
+
+// const fa = () => {
+//   var hello = "hello";
+// };
+// console.log(hello);
+// for (let i = 0; i < 10; i++) {
+//   var helloq = "hello";
+//   console.log(i);
+// }
+// console.log(helloq);
+
+// var is function scoped
+
+// function hello() {
+//   const hello = "hello";
+//   let me = "nahin";
+// }
+// console.log(hello);
+//
+// if (true) {
+//   const hello = "hello";
+//   let me = "nahin";
+// }
+// console.log(hello);
+
+// let val1 = 10;
+// let val2 = 10;
+//
+// console.log(khela);
+// console.log(result);
+// console.log(result2);
+
+// function add(num1, num2) {
+//   const total = num1 + num2;
+//   return total;
+// }
+// const khela = "football";
+// let result = add(val1, val2);
+// var result2 = add(23, 67);
+
+// Hoisting
+
+// let a = 10;
+// a = 23;
+// console.log(a);
+
+// const a;
+// a = 1;
+// console.log(a);
+
+// let a;
+// console.log(a);
---
+++ javascript/09_loop.js
@@ -0,0 +1,41 @@
+// let i = 0;
+// for (; ; i++) {
+//   console.log(i);
+// }
+
+// const person = {
+//   name: "Zinia",
+//   age: 21,
+//   height: 0,
+//   weight: 0,
+// };
+
+const person = [2, 1, 3, 4, 5];
+
+// for (let key in person) {
+//   console.log(person[key]);
+// }
+//
+
+// for eachEach basic way to implementation
+// person.forEach(myfunction);
+//
+// function myfunction(value, index, array) {
+//   console.log(value);
+//   console.log(array);
+// }
+
+// person.forEach(function myfunction(value, index, array) {
+//   console.log(value);
+//   console.log(array);
+// });
+
+// person.forEach(function (value, index, array) {
+//   console.log(value);
+//   console.log(array);
+// });
+
+// person.forEach((value, index, array) => {
+//   console.log(value);
+//   console.log(array);
+// });
